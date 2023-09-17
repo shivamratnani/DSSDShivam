1af43ef82c243638ec273d3870de47fba92be1d1
Shivam Ratnani <shivamratnani@Shivams-MacBook-Air.local>
2023-09-17 16:28:59 -0500

    https://dssd-shivam.onrender.com added

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -3,7 +3,7 @@ const graphDiv2 = document.getElementById("graph2");
 const graphDiv3 = document.getElementById("graph3");
 
 fetch(
-    "http://localhost:3000/g1" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "https://dssd-shivam.onrender.com/g1" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
     Plotly.newPlot( graphDiv, [ {
         ...(await res.json()),
@@ -12,7 +12,7 @@ fetch(
 })
 
 fetch(
-    "http://localhost:3000/g2" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "https://dssd-shivam.onrender.com/g2" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
     Plotly.newPlot( graphDiv2, [ {
         ...(await res.json()),
@@ -21,7 +21,7 @@ fetch(
 })
 
 fetch(
-    "http://localhost:3000/g3" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "https://dssd-shivam.onrender.com/g3" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
     Plotly.newPlot( graphDiv3, [ {
         ...(await res.json()),
