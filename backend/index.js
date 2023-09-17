c24bb6b34e4500b9f7c9d125009ef04c26fa165f
Shivam Ratnani <shivamratnani@Shivams-MacBook-Air.local>
2023-09-17 16:22:59 -0500

    Edited DSSD Project

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,72 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import csv from "csv-parser";
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: "http://localhost:5500"
-}))
+}));
 
-app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
-    }).status(200)
-})
+app.get("/g1", (req, res) => {
+    const dataPoints = { x: [], y: [] };
+
+    fs.createReadStream('/Users/shivamratnani/DSSDShivam/spotify-2023.csv') 
+        .pipe(csv())
+        .on('data', row => {
+            dataPoints.x.push(row.released_year);
+            dataPoints.y.push(row.streams);
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+            res.status(200).send(dataPoints);
+        })
+        .on('error', (err) => {
+            console.error(err);
+            res.status(500).send('Could not read CSV file');
+        });
+});
+
+app.get("/g2", (req, res) => {
+    const dataPoints = { x: [], y: [] };
+
+    fs.createReadStream('/Users/shivamratnani/DSSDShivam/spotify-2023.csv') 
+        .pipe(csv())
+        .on('data', row => {
+            dataPoints.x.push(row.released_month);
+            dataPoints.y.push(row.streams);
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+            res.status(200).send(dataPoints);
+        })
+        .on('error', (err) => {
+            console.error(err);
+            res.status(500).send('Could not read CSV file');
+        });
+});
+
+app.get("/g3", (req, res) => {
+    const dataPoints = { x: [], y: [] };
+
+    fs.createReadStream('/Users/shivamratnani/DSSDShivam/spotify-2023.csv') 
+        .pipe(csv())
+        .on('data', row => {
+            dataPoints.x.push(row.released_day);
+            dataPoints.y.push(row.streams);
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+            res.status(200).send(dataPoints);
+        })
+        .on('error', (err) => {
+            console.error(err);
+            res.status(500).send('Could not read CSV file');
+        });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port} 🚀`));
 
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`server listening on port ${port} 🚀`))
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,31 @@
 const graphDiv = document.getElementById("graph");
+const graphDiv2 = document.getElementById("graph2");
+const graphDiv3 = document.getElementById("graph3");
 
 fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "http://localhost:3000/g1" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
+    Plotly.newPlot( graphDiv, [ {
+        ...(await res.json()),
+        type: "bar"
+    } ]);
+})
+
+fetch(
+    "http://localhost:3000/g2" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+).then(async res => {
+    Plotly.newPlot( graphDiv2, [ {
+        ...(await res.json()),
+        type: "bar"
+    } ]);
+})
+
+fetch(
+    "http://localhost:3000/g3" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+).then(async res => {
+    Plotly.newPlot( graphDiv3, [ {
+        ...(await res.json()),
+        type: "bar"
+    } ]);
 })
 
